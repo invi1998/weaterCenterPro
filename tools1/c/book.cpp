d74de1b8fa3f9bab523619d93bbb4e82ef19d323
invi1998 <2267661826@qq.com>
2022-03-25 10:57:23 +0800

    使用二值信号量给共享内存加锁

--- tools1/c/book.cpp
+++ tools1/c/book.cpp
@@ -4,6 +4,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include "_public.h"
+
+// 创建一个信号量对象，用于给共享内存加锁
+CSEM sem;
 
 struct st_pid
 {
@@ -27,6 +31,19 @@ int main(int argc, char* argv[])
         printf("shmget(0x5005, sizeof(struct st_pid), 0640|IPC_CREAT) faild \n");
     }
 
+    // 初始化信号量，第一个参数key：是和信号量那个key是一样的，也是一个十六进制数，注意这里这个key也可以使用共享内存那个key,他们两个是独立的，互不冲突
+    // 第二个参数：value 表示信号量的初始值，而知信号量的初始值缺省为1
+    // 第三个参数，信号量的标志 如果是二值信号量的化就是 SEM_UNDO 也是缺省的 
+    // （所以可以看出，我们提供的这个CSEM 信号量类，默认情况下是一个二值信号量）
+    // 如果信号量已经存在，获取信号信号量，如果不存在，则创建它并初始化为value
+    if(sem.init(0x5005) == false)
+    {
+        printf("sem.init(0x5005) faild \n");
+        return -1;
+    }
+
+    // 然后编写加锁和解锁的代码
+
     // 先定义一个指针变量用于存放 shmat 函数的返回地址。（用于指向共享内存的结构体白能量指针）
     struct st_pid* stpid = nullptr;     // 初始化为 0 或者 NULL 或者在c++11中可以初始化为 nullptr
 
@@ -36,8 +53,15 @@ int main(int argc, char* argv[])
     {
         printf("shmat(shmid, 0, 0) faild \n");
     }
-
     // 然后就可以像使用普通的指针一样使用共享内存了
+
+    // 在加锁之前将当前时间 和 信号量的值显示出来
+    printf("start  time = %ld， value = %d \n", time(0), sem.value());
+
+    sem.P();        // 加锁
+
+    // 在加锁之后将当前时间 和 信号量的值显示出来
+    printf("start  time = %ld， value = %d \n", time(0), sem.value());
 
     // 在赋值之前我们先把共享内存里面的东西读出来
     printf("pid = %d， name = %s \n", stpid->pid, stpid->name);
@@ -48,6 +72,15 @@ int main(int argc, char* argv[])
 
     // 在赋值之后我们也把共享内存里面的东西读出来
     printf("pid = %d， name = %s \n", stpid->pid, stpid->name);
+    sleep(10);
+
+    // 在解锁之前将当前时间 和 信号量的值显示出来
+    printf("end  time = %ld， value = %d \n", time(0), sem.value());
+
+    sem.V();        // 解锁
+
+    // 在解锁之后将当前时间 和 信号量的值显示出来
+    printf("end  time = %ld， value = %d \n", time(0), sem.value());
 
 
     // 把共享内存从当前进程中分离
