bbed29090bb659d560d888412ec2f83fa68dad26
invi1998 <2267661826@qq.com>
2022-04-02 11:09:58 +0800

    压缩文件模块开发完成

--- tools1/c/gzipfiles.cpp
+++ tools1/c/gzipfiles.cpp
@@ -46,6 +46,7 @@ int main(int argc, char* argv[])
         return -1;
     }
 
+    char strCmd[1024];      // 存放 gzip 压缩文件的命令
     // 然后就是遍历目录中的文件名
     while(true)
     {
@@ -56,12 +57,28 @@ int main(int argc, char* argv[])
             break;
         }
 
-        printf("DirName=%s, FileName=%s, FullFileName=%s, FileSize=%d, ModifyTime=%s, CreateTime=%s, AccessTime=%s\n",\
-                Dir.m_DirName, Dir.m_FileName, Dir.m_FullFileName, Dir.m_FileSize, Dir.m_ModifyTime, Dir.m_CreateTime, Dir.m_AccessTime);
+        // printf("DirName=%s, FileName=%s, FullFileName=%s, FileSize=%d, ModifyTime=%s, CreateTime=%s, AccessTime=%s\n",\
+        //         Dir.m_DirName, Dir.m_FileName, Dir.m_FullFileName, Dir.m_FileSize, Dir.m_ModifyTime, Dir.m_CreateTime, Dir.m_AccessTime);
 
         // 然后把文件的时间和超时时间点做比较，如果文件时间更早，就说明是一个历史文件，就需要进行压缩
+        // 注意：这里不仅要和时间点做比较，还要和文件名做比（通过文件名来判断是否是已经被压缩过的文件，压缩过的文件就需要进行压缩了）
+        // MatchStr 是框架里的一个用于比较字符串是否匹配的函数
+        if((strcmp(Dir.m_ModifyTime, strTimeOut)) < 0 && (MatchStr(Dir.m_FileName, "*.gz") == false))
+        {
+            // 压缩文件，调用操作系统的gzip命令压缩文件
+            // SNPRINTF 函数是框架里的安全函数，功能和snprintf是一样的（将可变参数 “…” 按照format的格式格式化为字符串，然后再将其拷贝至str中。）
+            // 1>/dev/null 2>/dev/null 这两各参数的写法是我们经常的写法，就是将标准输出和标准错误都定位为空（黑洞），也就是不用输出任何东西
+            SNPRINTF(strCmd, sizeof(strCmd), 1000, "/usr/bin/gzip -f %s 1>/dev/null 2>/dev/null", Dir.m_FullFileName);
+            if(system(strCmd) == 0)
+            {
+                printf("gzip %s ok.\n", Dir.m_FullFileName);
+            }
+            else
+            {
+                printf("gzip %s faild.\n", Dir.m_FullFileName);
+            }
+        }
 
-        // 压缩文件，调用操作系统的gzip命令压缩文件
     }
 
     return 0;
