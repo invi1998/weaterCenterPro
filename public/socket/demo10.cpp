7791fef184b270aaad4701b83f59fda45970f12b
invi1998 <2267661826@qq.com>
2022-04-06 15:51:52 +0800

    整理代码

--- public/socket/demo10.cpp
+++ public/socket/demo10.cpp
@@ -13,95 +13,41 @@
  * author：invi
 */
 #include "../_public.h"
-
-CLogFile logfile;      // 服务程序的运行日志。
-CTcpServer TcpServer;  // 创建服务端对象。
-
-void FathEXIT(int sig);  // 父进程退出函数。
-void ChldEXIT(int sig);  // 子进程退出函数。
  
 int main(int argc,char *argv[])
 {
-  if (argc!=3)
+  if (argc!=2)
   {
-    printf("Using:./demo10 port logfile\nExample:./demo10 5005 /tmp/demo10.log\n\n"); return -1;
+    printf("Using:./demo08 port\nExample:./demo08 5005\n\n"); return -1;
   }
 
-  // 关闭全部的信号和输入输出。
-  // 设置信号,在shell状态下可用 "kill + 进程号" 正常终止些进程
-  // 但请不要用 "kill -9 +进程号" 强行终止
-  CloseIOAndSignal();
-  signal(SIGINT,FathEXIT);
-  signal(SIGTERM,FathEXIT);
-
-  if (logfile.Open(argv[2],"a+")==false) { printf("logfile.Open(%s) failed.\n",argv[2]); return -1; }
+  CTcpServer TcpServer;
 
   // 服务端初始化。
   if (TcpServer.InitServer(atoi(argv[1]))==false)
   {
-    logfile.Write("TcpServer.InitServer(%s) failed.\n",argv[1]); return -1;
+    printf("TcpServer.InitServer(%s) failed.\n",argv[1]); return -1;
   }
 
-  while (true)
+  // 等待客户端的连接请求。
+  if (TcpServer.Accept()==false)
   {
-    // 等待客户端的连接请求。
-    if (TcpServer.Accept()==false)
-    {
-      logfile.Write("TcpServer.Accept() failed.\n"); FathEXIT(-1);
-    }
-
-    logfile.Write("客户端（%s）已连接。\n",TcpServer.GetIP());
-
-    if (fork()>0) { TcpServer.CloseClient(); continue; }  // 父进程继续回到Accept()。
-   
-    // 子进程重新设置退出信号。
-    signal(SIGINT,ChldEXIT); signal(SIGTERM,ChldEXIT);
-
-    TcpServer.CloseListen();
-
-    // 子进程与客户端进行通讯，处理业务。
-    char buffer[102400];
-
-    // 与客户端通讯，接收客户端发过来的报文后，回复ok。
-    while (1)
-    {
-      memset(buffer,0,sizeof(buffer));
-      if (TcpServer.Read(buffer)==false) break; // 接收客户端的请求报文。
-      logfile.Write("接收：%s\n",buffer);
-
-      strcpy(buffer,"ok");
-      if (TcpServer.Write(buffer)==false) break; // 向客户端发送响应结果。
-      logfile.Write("发送：%s\n",buffer);
-    }
-
-    ChldEXIT(0);
+    printf("TcpServer.Accept() failed.\n"); return -1;
   }
-}
 
-// 父进程退出函数。
-void FathEXIT(int sig)  
-{
-  // 以下代码是为了防止信号处理函数在执行的过程中被信号中断。
-  signal(SIGINT,SIG_IGN); signal(SIGTERM,SIG_IGN);
+  printf("客户端（%s）已连接。\n",TcpServer.GetIP());
 
-  logfile.Write("父进程退出，sig=%d。\n",sig);
+  char buffer[102400];
 
-  TcpServer.CloseListen();    // 关闭监听的socket。
+  // 与客户端通讯，接收客户端发过来的报文后，回复ok。
+  while (1)
+  {
+    memset(buffer,0,sizeof(buffer));
+    if (TcpServer.Read(buffer)==false) break; // 接收客户端的请求报文。
+    printf("接收：%s\n",buffer);
 
-  kill(0,15);     // 通知全部的子进程退出。
-
-  exit(0);
-}
-
-// 子进程退出函数。
-void ChldEXIT(int sig)  
-{
-  // 以下代码是为了防止信号处理函数在执行的过程中被信号中断。
-  signal(SIGINT,SIG_IGN); signal(SIGTERM,SIG_IGN);
-
-  logfile.Write("子进程退出，sig=%d。\n",sig);
-
-  TcpServer.CloseClient();    // 关闭客户端的socket。
-
-  exit(0);
+    strcpy(buffer,"ok");
+    if (TcpServer.Write(buffer)==false) break; // 向客户端发送响应结果。
+    printf("发送：%s\n",buffer);
+  }
 }
