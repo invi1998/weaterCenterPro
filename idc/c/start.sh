82ca628244037e3fe9a9f4679385d9d35cf04b4c
invi1998 <2267661826@qq.com>
2022-04-10 20:54:56 +0800

    程序运行shell脚本添加tcp文件传输服务

--- idc/c/start.sh
+++ idc/c/start.sh
@@ -15,10 +15,10 @@
 /project/tools/bin/procctl 300 /project/tools/bin/deletefiles /tmp/idc/surfdata "*" 0.02
 
 # 下载全国气象站点观测的分钟数据的xml文件
-/project/tools/bin/procctl 30 /project/tools/bin/ftpgetfiles /log/idc/ftpgetfiles_surfdata.log "<host>127.0.0.1:21</host><mode>1</mode><username>invi</username><password>sh269jgl105</password><localpath>/idcdata/surfdata</localpath><remotepath>/tmp/idc/surfdata</remotepath><matchname>SURF_ZH*.XML</matchname><listfilename>/idcdata/ftplist/ftpgetfiles_surfdata.list</listfilename><ptype>1</ptype><remotepathbak>/tmp/idc/surfdatabak</remotepathbak><okfilename>/idcdata/ftplist/ftpgetfiles_surfdata.xml</okfilename><checkmtime>true</checkmtime><timeout>80</timeout><pname>ftpgetfiles_surfdata</pname>"
+/project/tools/bin/procctl 30 /project/tools/bin/ftpgetfiles /log/idc/ftpgetfiles_surfdata.log "<host>192.168.31.166:21</host><mode>1</mode><username>invi</username><password>sh269jgl105</password><localpath>/idcdata/surfdata</localpath><remotepath>/tmp/idc/surfdata</remotepath><matchname>SURF_ZH*.XML</matchname><listfilename>/idcdata/ftplist/ftpgetfiles_surfdata.list</listfilename><ptype>1</ptype><remotepathbak>/tmp/idc/surfdatabak</remotepathbak><okfilename>/idcdata/ftplist/ftpgetfiles_surfdata.xml</okfilename><checkmtime>true</checkmtime><timeout>80</timeout><pname>ftpgetfiles_surfdata</pname>"
 
 # 上传全国气象站点观测的分钟数据的xml文件
-/project/tools/bin/procctl 30 /project/tools/bin/ftpputfiles /log/idc/ftpputfiles_surfdata.log "<host>127.0.0.1:21</host><mode>1</mode><username>invi</username><password>sh269jgl105</password><localpath>/tmp/idc/surfdata</localpath><remotepath>/tmp/ftpputest</remotepath><matchname>SURF_ZH*.XML</matchname><ptype>1</ptype><localpathbak>/tmp/idc/surfdatabak</localpathbak><okfilename>/idcdata/ftplist/ftpputfiles_surfdata.xml</okfilename><timeout>80</timeout><pname>ftpputfiles_surfdata</pname>"
+/project/tools/bin/procctl 30 /project/tools/bin/ftpputfiles /log/idc/ftpputfiles_surfdata.log "<host>192.168.31.166:21</host><mode>1</mode><username>invi</username><password>sh269jgl105</password><localpath>/tmp/idc/surfdata</localpath><remotepath>/tmp/ftpputest</remotepath><matchname>SURF_ZH*.XML</matchname><ptype>1</ptype><localpathbak>/tmp/idc/surfdatabak</localpathbak><okfilename>/idcdata/ftplist/ftpputfiles_surfdata.xml</okfilename><timeout>80</timeout><pname>ftpputfiles_surfdata</pname>"
 
 # 清理采集的全国气象站点观测的分钟数据目录/idcdata/surfdata中的历史数据文件。
 /project/tools/bin/procctl 300 /project/tools/bin/deletefiles /idcdata/surfdata "*" 0.02
@@ -26,4 +26,14 @@
 # 清理采集的全国气象站点观测的分钟数据目录/tmp/ftpputest中的历史数据文件。
 /project/tools/bin/procctl 300 /project/tools/bin/deletefiles /tmp/ftpputest "*" 0.02
 
+# 文件传输的服务端程序。
+/project/tools/bin/procctl 10 /project/tools/bin/fileserver 5005 /log/idc/fileserver.log
 
+# 把目录/tmp/ftpputest中的文件上传到/tmp/tcpputest目录中。
+/project/tools/bin/procctl 20 /project/tools/bin/tcpputfiles /log/idc/tcpputfiles_surfdata.log "<ip>192.168.31.166</ip><port>5005</port><ptype>1</ptype><clientpath>/tmp/ftpputest</clientpath><andchild>true</andchild><matchname>*.XML,*.CSV,*.JSON</matchname><srvpath>/tmp/tcpputest</srvpath><timetvl>10</timetvl><timeout>50</timeout><pname>tcpputfiles_surfdata</pname>"
+
+# 把目录/tmp/tcpputest中的文件下载到/tmp/tcpgetest目录中。
+/project/tools/bin/procctl 20 /project/tools/bin/tcpgetfiles /log/idc/tcpgetfiles_surfdata.log "<ip>192.168.31.166</ip><port>5005</port><ptype>1</ptype><srvpath>/tmp/tcpputest</srvpath><andchild>true</andchild><matchname>*.XML,*.CSV,*.JSON</matchname><clientpath>/tmp/tcpgetest</clientpath><timetvl>10</timetvl><timeout>50</timeout><pname>tcpgetfiles_surfdata</pname>"
+
+# 清理采集的全国气象站点观测的分钟数据目录/tmp/tcpgetest中的历史数据文件。
+/project/tools/bin/procctl 300 /project/tools/bin/deletefiles /tmp/tcpgetest "*" 0.02
