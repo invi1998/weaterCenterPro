72d5c7b59b2ba089e9030823871a3e8fd1376f8d
invi1998 <2267661826@qq.com>
2022-04-20 23:10:17 +0800

    将数据同步加入运行脚本

--- idc/c/killall.sh
+++ idc/c/killall.sh
@@ -4,9 +4,9 @@
 
 killall -9 procctl
 killall gzipfiles crtsurfdata deletefiles ftpgetfiles ftpputfiles tcpputfiles tcpgetfiles fileserver obtcodetodb
-killall execsql dminingmysql xmltodb
+killall execsql dminingmysql xmltodb syncupdate syncincrement
 
 sleep 3
 
 killall -9 gzipfiles crtsurfdata deletefiles ftpgetfiles ftpputfiles tcpputfiles tcpgetfiles fileserver obtcodetodbobtcodetodb\
-killall execsql dminingmysql xmltodb
+killall execsql dminingmysql xmltodb syncupdate syncincrement
