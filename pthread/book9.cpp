b08a45c68abc7f1d7db44ff2d2d5f87cabcc1df8
invi1998 <2267661826@qq.com>
2022-04-29 19:07:20 +0800

    线程清理函数

--- pthread/book9.cpp
+++ pthread/book9.cpp
@@ -5,19 +5,21 @@
 #include <unistd.h>
 #include <pthread.h>
 
+// 线程入口函数
 void * thmain1(void * arg);
+
+// 线程清理函数
+void thcleanup1(void* arg);
+void thcleanup2(void* arg);
+void thcleanup3(void* arg);
 
 int main(int argc, char* argv[])
 {
 
     pthread_t thid1=0;
 
-    pthread_attr_t attr;        // 声明线程属性的数据结构
-    pthread_attr_init(&attr);   // 初始化
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);        // 设置线程的分离属性
-
     // 创建线程（将线程属性作为参数传递给创建线程函数）
-    if(pthread_create(&thid1, &attr, thmain1, NULL) != 0)
+    if(pthread_create(&thid1, NULL, thmain1, NULL) != 0)
     {
         printf("线程创建失败\n");
         exit(-1);
@@ -37,10 +39,40 @@ int main(int argc, char* argv[])
 
 void * thmain1(void * arg)
 {
+    // 在线程入口函数中将线程清理函数入栈
+    pthread_cleanup_push(thcleanup1, NULL);
+    pthread_cleanup_push(thcleanup2, NULL);
+    pthread_cleanup_push(thcleanup3, NULL);
+
     for(int i = 0; i < 3; i++)
     {
         sleep(1);
         printf("线程1-【%d】\n", i);
     }
+
+    pthread_cleanup_pop(1);
+    pthread_cleanup_pop(0);
+    pthread_cleanup_pop(1);
     return (void*)10;
+}
+
+// 线程清理函数
+void thcleanup1(void* arg)
+{
+    // 在这里释放资源，关闭文件，断开连接，回滚事务，释放锁等
+    printf("cleanup 1 ok\n");
+}
+
+// 线程清理函数
+void thcleanup2(void* arg)
+{
+    // 在这里释放资源，关闭文件，断开连接，回滚事务，释放锁等
+    printf("cleanup 2 ok\n");
+}
+
+// 线程清理函数
+void thcleanup3(void* arg)
+{
+    // 在这里释放资源，关闭文件，断开连接，回滚事务，释放锁等
+    printf("cleanup 3 ok\n");
 }
